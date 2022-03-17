e7edea650ddc89a1183d9084b155ba3c2ba1005d
franzannakarolina <nerdstecnologia0@gmail.com>
2022-03-17 20:33:17 -0300

    fix fleet vehicle

--- fleet_rest_api/datamodels/vehicle_odometer.py
+++ fleet_rest_api/datamodels/vehicle_odometer.py
@@ -9,10 +9,10 @@ class VehicleOdometer(Datamodel):
 
     id = fields.Integer(required=True)
     name = fields.String(required=True)
-    value = fields.Float(required=True)
-    vehicle_id = fields.Integer(required=True)
+    # value = fields.Integer(required=True)
+    # vehicle_id = fields.Integer(required=True)
     unit = fields.String(required=True)
-    driver_id = fields.Integer(required=True)
+    # driver_id = fields.Integer(required=True)
     sequence = fields.Integer(required=True)
     write_date = fields.DateTime(required=True)
     
