bde41434b9f83428feb7908696102387c38211fc
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-03-19 16:14:34 -0300

    [FIX] fix and refactor vehicle_assignation_log services

--- fleet_rest_api/datamodels/vehicle_assignation_log.py
+++ fleet_rest_api/datamodels/vehicle_assignation_log.py
@@ -10,7 +10,35 @@ class FleetVehicleAssignationLog(Datamodel):
     _name = "fleet.vehicle.assignation.log"
     _description = "Services for vehicle assignation log"
 
-    id = fields.Integer(required=True, allow_none=False)
-    description = fields.String(required=True, allow_none=False)
-    date_start = fields.Date(string="Start Date")
-    date_end = fields.Date(string="End Date")
+    id = fields.Integer(required=False, allow_none=False)
+    vehicle_id = fields.Integer(required=True, allow_none=False)
+    driver_id = fields.Integer(required=True, allow_none=False)
+    date_start = fields.Date()
+    date_end = fields.Date()
+
+
+class FleetVehicleAssignationLogSearchParam(Datamodel):
+    _name = "fleet.vehicle.assignation.log.search.param"
+
+    id = fields.Integer(required=False, allow_none=False)
+    vehicle_id = fields.Integer(required=False, allow_none=False)
+    driver_id = fields.Integer(required=False, allow_none=False)
+
+
+class FleetVehicleAssignationLogCreateParam(Datamodel):
+    _name = "fleet.vehicle.assignation.log.create.param"
+
+    vehicle_id = fields.Integer(required=False, allow_none=False)
+    driver_id = fields.Integer(required=False, allow_none=False)
+    date_start = fields.Date()
+    date_end = fields.Date()
+
+
+class FleetVehicleAssignationLogUpdateParam(Datamodel):
+    _name = "fleet.vehicle.assignation.log.update.param"
+
+    id = fields.Integer(required=False, allow_none=False)
+    vehicle_id = fields.Integer(required=False, allow_none=False)
+    driver_id = fields.Integer(required=False, allow_none=False)
+    date_start = fields.Date()
+    date_end = fields.Date()
