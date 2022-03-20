98819531147a5e3969eaf940223ed5bcdbc21ab3
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-03-20 04:29:38 -0300

    [RFC] fleet_rest_api: refactor datamodels and services fleet_service_type fleet_vehicle_log_contract

---
+++ fleet_rest_api/datamodels/fleet_service_type_datamodels.py
@@ -0,0 +1,43 @@
+# Copyright 2022 - TODAY, Marcel Savegnago <marcel.savegnago@escodoo.com.br>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo.addons.datamodel import fields
+from odoo.addons.datamodel.core import Datamodel
+
+
+class FleetServiceTypeBase(Datamodel):
+    _name = "fleet.service.type.base"
+
+    id = fields.Integer(required=False, allow_none=False)
+    name = fields.String(required=False, allow_none=True)
+    category = fields.String(required=False, allow_none=False)
+
+
+class FleetServiceTypeInput(Datamodel):
+    _name = "fleet.service.type.input"
+    _inherit = ["fleet.service.type.base"]
+
+
+class FleetServiceTypeOutput(Datamodel):
+    _name = "fleet.service.type.output"
+    _inherit = ["fleet.service.type.base"]
+
+
+class FleetServiceTypeSearchInput(Datamodel):
+    _name = "fleet.service.type.search.input"
+
+    id = fields.Integer(required=False, allow_none=False)
+    name = fields.String(required=False, allow_none=False)
+    category = fields.String(required=False, allow_none=False)
+
+
+class FleetServiceTypeSearchOutput(Datamodel):
+    _name = "fleet.service.type.search.output"
+
+    size = fields.Integer(required=True, allow_none=False)
+    data = fields.NestedModel(
+        "fleet.service.type.output",
+        required=False,
+        allow_none=True,
+        many=True,
+    )
