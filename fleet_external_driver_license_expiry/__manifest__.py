9940a1abf54e6bed4c6fd1c8d3705e3207e611c0
Wesley Oliveira <wesleygabrieldeoliveira@gmail.com>
2026-04-23 09:23:01 -0300

    [ADD] fleet_external_driver_license_expiry: add new module

---
+++ fleet_external_driver_license_expiry/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2026 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "External Driver License Expiry",
+    "summary": """
+        Module to track external driver's license expiration""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/fleet-addons",
+    "depends": ["fleet_external_driver"],
+    "maintainers": ["WesleyOliveira98"],
+    "data": ["views/res_partner.xml"],
+    "application": False,
+    "installable": True,
+}
